32ccc61c62e442635304719a74f19e9682b3ae35
Arjun <arjun_p_uk@yahoo.co.uk>
2025-02-26 22:00:12 +0000

    adding a jupyter notebook explanation and cleaning up organisation of repo

--- Monte Carlo simulation/MonteCarlo.py
+++ Monte Carlo simulation/MonteCarlo.py
@@ -5,28 +5,27 @@ import datetime as dt
 from pandas_datareader import data as pdr
 
 def get_data(stocks, start, end):
-    stockData = pd.concat(
-    {stock: pdr.DataReader(stock, "stooq")["Close"] for stock in stocks}, 
-    axis=1)
-    #print(stockData)
-    stockData = stockData.iloc[::-1]
+    # old way of getting data
+    # stockData = pd.concat(
+    # {stock: pdr.DataReader(stock, "stooq")["Close"] for stock in stocks}, 
+    # axis=1)
+    # stockData = stockData.iloc[::-1]
+    # stockData.index = pd.to_datetime(stockData.index)
+    # stockData = stockData.loc[start:end]
 
-    stockData.index = pd.to_datetime(stockData.index)
-    stockData = stockData.loc[start:end]
-    print(stockData)
+    stockData = pdr.get_data_stooq(stocks, start, end)
+    stockData = stockData["Close"]
     returns = stockData.pct_change()
     meanReturns = returns.mean()
     covMatrix = returns.cov()
     return meanReturns, covMatrix
 
 stockList = ["AAPL", "MSFT", "GOOGL"]
-#stocks = [stock + ".AX" for stock in stockList]
 endDate = dt.datetime.now()
 startDate = endDate - dt.timedelta(days=300)
 
 meanReturns, covMatrix = get_data(stockList, startDate, endDate)
 print(meanReturns)
-#print(covMatrix)
 
 #give the stocks a random weight and normalise it to lie within [0,1]. 
 weights = np.random.random(len(meanReturns))
@@ -38,7 +37,7 @@ timeframe = 100
 
 meanM = np.full(shape=(timeframe, len(weights)), fill_value=meanReturns)
 meanM = meanM.T
-portfolio_sims = np.full(shape=(timeframe, numSimulations), fill_value=0.0)
+portfolioSimulations = np.full(shape=(timeframe, numSimulations), fill_value=0.0)
 initialPortfolio = 10000
 #MC loop
 for m in range(0, numSimulations):
@@ -47,9 +46,9 @@ for m in range(0, numSimulations):
     Z = np.random.normal(size=(timeframe, len(weights)))
     L = np.linalg.cholesky(covMatrix)
     dailyReturns = meanM + np.inner(L, Z)
-    portfolio_sims[:,m] = np.cumprod(np.inner(weights, dailyReturns.T)+1) * initialPortfolio
+    portfolioSimulations[:,m] = np.cumprod(np.inner(weights, dailyReturns.T)+1) * initialPortfolio
 
-plt.plot(portfolio_sims)
+plt.plot(portfolioSimulations)
 plt.ylabel("Portfolio Value ($)")
 plt.xlabel("Days")
 plt.title("Monte Carlo simulation of a stock portfolio")
